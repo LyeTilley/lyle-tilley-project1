3a5678248ecdae18db26056729cd137103dd010a
Your <tilley_l@hotmail.com>
2021-06-03 09:02:58 -0300

    added final touches to assignment 2

--- styles/js/main.js
+++ styles/js/main.js
@@ -1,39 +1,36 @@
 alert('Welcome to Lyles Boat Ride Adventures!');
 
+
+// slide here
+
 const slides = document.querySelectorAll('.slide');
 const next = document.querySelector('#next');
 const prev = document.querySelector('#prev');
-const auto = false; // Auto scroll
+const auto = false; // scroll yourself here
 const intervalTime = 5000;
 let slideInterval;
 
 const nextSlide = () => {
-  // Get current class
+  
   const current = document.querySelector('.current');
-  // Remove current class
   current.classList.remove('current');
-  // Check for next slide
+  
   if (current.nextElementSibling) {
-    // Add current to next sibling
     current.nextElementSibling.classList.add('current');
   } else {
-    // Add current to start
     slides[0].classList.add('current');
   }
   setTimeout(() => current.classList.remove('current'));
 };
 
 const prevSlide = () => {
-  // Get current class
   const current = document.querySelector('.current');
-  // Remove current class
   current.classList.remove('current');
-  // Check for prev slide
+  
   if (current.previousElementSibling) {
-    // Add current to prev sibling
     current.previousElementSibling.classList.add('current');
   } else {
-    // Add current to last
+    
     slides[slides.length - 1].classList.add('current');
   }
   setTimeout(() => current.classList.remove('current'));
@@ -55,6 +52,8 @@ prev.addEventListener('click', e => {
     slideInterval = setInterval(nextSlide, intervalTime);
   }
 });
+
+
 
 // Auto slide
 if (auto) {
